ad3b49aeb97a7c8a0f05a282a227e28899c67160
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2026-05-22 16:15:46 +0200

    test: server test

--- backend/test/server.test.js
+++ backend/test/server.test.js
@@ -1,18 +1,18 @@
 const request = require('supertest');
 describe('API with cache', () => {
 
-    test('GET /post', async () => {
-
-        for (let i = 0; i < 10; i++) {
-            const res = await request('http://localhost:3000').get('/posts');
-            expect(res.statusCode).toBe(200);
-        }
-    });
-
     test('GET /post-non-opt', async () => {
 
         for (let i = 0; i < 10; i++) {
             const res = await request('http://localhost:3000').get('/posts-non-opt');
+            expect(res.statusCode).toBe(200);
+        }
+    });
+
+    test('GET /post', async () => {
+
+        for (let i = 0; i < 10; i++) {
+            const res = await request('http://localhost:3000').get('/posts');
             expect(res.statusCode).toBe(200);
         }
     });
